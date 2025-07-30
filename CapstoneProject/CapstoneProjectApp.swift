5019fd3e91b698390d8cba8f37c4ecd35ad14aa4
Vachi Kalra <vachikalra@gmail.com>
2025-07-30 10:37:22 -0700

    Initial Commit

---
+++ CapstoneProject/CapstoneProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneProjectApp.swift
+//  CapstoneProject
+//
+//  Created by Vachi Kalra on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CapstoneProjectTests/CapstoneProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneProjectTests.swift
+//  CapstoneProjectTests
+//
+//  Created by Vachi Kalra on 7/30/25.
+//
+
+import Testing
+@testable import CapstoneProject
+
+struct CapstoneProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
